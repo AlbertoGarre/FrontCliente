bc30fa5d4f2d983a293eece9f1f7d62edba15a61
alberto <alberto_garre@hotmail.com>
2021-12-07 17:11:47 +0100

    Tarifas 2

--- src/components/TarjetaTarifa.js
+++ src/components/TarjetaTarifa.js
@@ -5,18 +5,18 @@ const TarjetaTarifa = ({ seleccionada, pulsa, tarifa}) => {
     return (
 
         
-            <div class="card mb-4 shadow-sm">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">{tarifa.titulo}</h4>
+            <div className="card mb-4 shadow-sm">
+                <div className="card-header">
+                    <h4 className="my-0 font-weight-normal">{tarifa.titulo}</h4>
                 </div>
-                <div class="card-body" onClick={() => pulsa(tarifa.id)} style={{ backgroundColor: seleccionada ? 'green':'white'  }}>
-                    <h1 class="card-title pricing-card-title"> <small class="text-muted ">{tarifa.Precio} / envío</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
+                <div className="card-body" onClick={() => pulsa(tarifa.id)} style={{ backgroundColor: seleccionada ? 'green':'white'  }}>
+                    <h1 className="card-title pricing-card-title"> <small className={(seleccionada ? 'text-white' : 'text-muted')}>{tarifa.Precio} / envío</small></h1>
+                    <ul className="list-unstyled mt-3 mb-4">
                         {tarifa.Cualidades.map((Cualidad) => (
-                            <li>{Cualidad}</li>
+                            <li className={(seleccionada ? 'text-white' : '')}>{Cualidad}</li>
                         ))}
                     </ul>
-                    <button type="button" style={{ backgroundColor: 'white' }} class="btn btn-lg btn-block btn-outline-primary ">Contrata ahora</button>
+                    <button type="button" style={{ backgroundColor: 'white' }} className={'btn btn-lg btn-block btn-outline-primary ' + (seleccionada ? 'text-success border-0 font-weight-bold' : 'text-muted')}>{seleccionada ? "Seleccionada":"Contrata ahora"}</button>
                 </div>
             </div>
 
