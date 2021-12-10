ae880acc7b6e0bd5a03c972ca481847781c40469
eduardogarre <eduardo.garre@gmail.com>
2021-12-10 15:38:15 +0100

    Colores corporativos en tarjetas de portada

--- src/components/Portada.js
+++ src/components/Portada.js
@@ -56,7 +56,7 @@ const Portada = ({ codigoSeguimiento, setCodigoSeguimiento, setPaginaActual }) =
                     </div>
                     <div className="col-lg-3 col-sm-6 mb-4">
                         <div className="card border-0 shadow ">
-                            <div className="card-body"> <i className="fas fa-cube h1 display-4 text-primary  mb-3"></i>
+                            <div className="card-body"> <i className="fas fa-cube h1 display-4 text-success  mb-3"></i>
                                 <h4 className="mt-4 mb-3">Calidad con total entrega</h4>
                                 <p>CONTOSOX es una compañía líder en el segmento de la mensajería exprés en España, Andorra y Portugal.
                                 </p>
@@ -65,7 +65,7 @@ const Portada = ({ codigoSeguimiento, setCodigoSeguimiento, setPaginaActual }) =
                     </div>
                     <div className="col-lg-3 col-sm-6 mb-4">
                         <div className="card border-0 shadow ">
-                            <div className="card-body"> <i className="fa fa-users h1 display-4 text-warning mb-3"></i>
+                            <div className="card-body"> <i className="fa fa-users h1 display-4 text-success mb-3"></i>
                                 <h4 className="mt-4 mb-3">Nuestro objetivo: tu satisfacción</h4>
                                 <p>Nuestro objetivo es desarrollar servicios que cumplan con las expectativas de nuestros clientes. </p>
                             </div>
@@ -74,7 +74,7 @@ const Portada = ({ codigoSeguimiento, setCodigoSeguimiento, setPaginaActual }) =
 
                     <div className="col-lg-3 col-sm-6 mb-4">
                         <div className="card border-0 shadow ">
-                            <div className="card-body"> <i className="fas fa-shuttle-van h1 display-4 text-danger  mb-3"></i>
+                            <div className="card-body"> <i className="fas fa-shuttle-van h1 display-4 text-success  mb-3"></i>
                                 <h4 className="mt-4 mb-3">Más que un transporte</h4>
                                 <p>Ofrecemos una amplia gama de servicios nacionales, internacionales y de valor añadido.</p>
                             </div>
