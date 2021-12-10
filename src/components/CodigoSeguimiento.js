a77571628a707f23bdc91a8d9e41f2ed9c8f8c02
eduardogarre <eduardo.garre@gmail.com>
2021-12-10 02:11:44 +0100

    Cambios cosméticos: texto botón de seguimiento

--- src/components/CodigoSeguimiento.js
+++ src/components/CodigoSeguimiento.js
@@ -37,7 +37,7 @@ const CodigoSeguimiento = ({ codigoSeguimiento, setCodigoSeguimiento }) => {
                     </div>
                     <div className="form-group ">
                         <button className="btn btn-success " type="submit">
-                            Seguir envío
+                            Sigue tu envío
                         </button>
                     </div>
                 </form>
